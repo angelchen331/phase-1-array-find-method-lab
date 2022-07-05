425816245021495c769c38c30939b8d966e36626
Yuchen Chen <angelchen3311@gmail.com>
2022-07-05 14:20:42 -0400

    commit tree d7b8b0b14ee555349bb92137a7fe816361680582

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+// function superbowlWin(record){
+//     const win = record.find(record => record.result === 'W')
+//     if (win.result === 'W'){
+//         return win.year;
+//     } else if {
+//         return (undefined)
+//     }
+// }
+
+
+function superbowlWin(record) {
+    const result = record.find(obj => obj.result === 'W')
+        return (result ? result.year : undefined);
+}
+
+// function superbowlWin(record){
+//     const sadReality = (record => record.result === "N/A" )
+//     if (sadReality.result === 'N/A'){
+//         return undefined;
+//     }
+// }
